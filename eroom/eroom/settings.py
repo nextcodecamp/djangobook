23ba05d40fb8728f56efeedd06d19a2608996e3e
nextcodecamp <nextcodecamp@gmail.com>
2025-02-08 15:36:55 +0700

    Update settings and urls
    Update settings and urls

--- eroom/eroom/settings.py
+++ eroom/eroom/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2!h!#j$quc$01qla&07gv4ybkpy!3_x8xubhu7=c0aj($65b0q"
+SECRET_KEY = "django-insecure-+n&@lr&+5h@%my8$f$v16i^g_sp90+l#9606@pt*8--^_)g23h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '93.127.206.75','194.238.18.26']
 
 
 # Application definition
@@ -34,32 +35,65 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
-    "django.contrib.sessions",
+    "django.contrib.sessions",    #session databse
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "vroom"
+    "django.contrib.redirects",
+    "django.contrib.sites",
+    "rest_framework",  #Rest API
+    
+    "rest_framework.authtoken",
+    'rest_framework_simplejwt',
+    'dbbackup',
+    "corsheaders",#Rest API
+    "vroom",
+    "elearning",
+    "myapp",   #"stock analylicts"
+    "ebook",
+    "blog",
+    "django_summernote"
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",  #for session 
+   
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.redirects.middleware.RedirectFallbackMiddleware",
+    'corsheaders.middleware.CorsMiddleware', #REST
+'django.middleware.common.CommonMiddleware', #REST
+
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True   #REST
+REST_FRAMEWORK = { 
+	'DEFAULT_AUTHENTICATION_CLASSES': [ 
+        		#'rest_framework.authentication.BasicAuthentication',
+                # 'rest_framework.authentication.TokenAuthentication', 	
+                #'rest_framework.authentication.SessionAuthentication', 
+                'rest_framework_simplejwt.authentication.JWTAuthentication',
+             
+    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+                 
+                  'rest_framework.permissions.IsAuthenticated',
+   ),
+}
+
 ROOT_URLCONF = "eroom.urls"
-STATIC_URL = "vroom/static/"   #Directory นี้จะอยู่ภัดจาก Directory Project -> eroom/vroom/static  
-STATICFILES_DIRS = [
-    "somewhere/static/",  #พาธอื่น ๆ ที่มี Static file ที่ต้องการให้ Django มองเห็น
-   
-]
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR.joinpath("templates"),
+            BASE_DIR.joinpath("vroom/templates"),
+          
+            
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -67,10 +101,13 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                'django.template.context_processors.media',   #For video allow reference in template directory
             ],
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = "eroom.wsgi.application"
 
@@ -109,20 +146,50 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
 USE_TZ = True
+#TIME_ZONE = "UTC"
 
+USE_I18N = False
+SITE_ID = 2
+USE_TZ = False
+
+DATETIME_FORMAT = '%d-%m-%Y %H:%M:%S' 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-#STATIC_URL = "static/" #กำหนดไว้ในแอป vroom หากกำหนดแบบนี้ต้องเก็บ static file ไว้ที่ folder eroom/static ในระดับ Project
-
+STATIC_URL = "vroom/static/"
+#os.path.join(BASE_DIR, 'app1/static/'),
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    "myapp/stock_img/",
+    "vroom/static/",
+    "elearning/static/video",
+    "elearning/static/img",
+    "myapp/static/"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+#EMAIL SETTINGS
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'contactnextcodecamp@gmail.com'
+EMAIL_HOST_PASSWORD = "qknl xzzc iwtm flhr"
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#IMG_URL = "img/"
+DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
+DBBACKUP_STORAGE_OPTIONS = {'location': BASE_DIR / 'backup'}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
--- eroom/eroom/urls.py
+++ eroom/eroom/urls.py
@@ -15,22 +15,135 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from  vroom import views    #Import view in vroom app to use in this page
+from django.urls import path, include
+from  vroom import views
+
+
+from vroom import views as vroom_view
+from elearning import views as elearning_view
+from myapp import views as stock_view 
+from ebook import views as ebook_view 
+from blog import views as blog_view
+from django.urls import include
+
+from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    #path("", elearning_view.home, name="home"),
+    #path("home/", elearning_view.home, name="home"),
+    path("aboutp/", elearning_view.aboutp, name="about page"),
+    path("contactp/", elearning_view.contactp, name="contact page"),
+    
+    path("analyticlab/",elearning_view.analytics_lab, name="analytics"),
+    #path("stock/<str:stock>/",stock_view.plot_line, name="plotline"),
+    
+    path("api/", include("vroom.urls")),
+    #path("elearning/", include("elearning.urls")),
+    #Topic 2.3.2
     path("admin/", admin.site.urls),
-    path("currentday/", views.current_datetime),
-    path("currentdatetime/", views.current_datetime_template),
-    path("videoform/", views.video_fill, name="videoform"),
-    path("thanks/", views.thanks, name="thanks"),
-    path("video_view/<int:id>/<int:year>/", views.video_view, name="listvideo"),
-    path("listvideo/", views.ListVideoView.as_view(), name="list_video"),
-    path("courselist/", views.course_list.as_view(), name="courselist"),
-    path("inquiryform/", views.inquiry_fill, name="inquiry"),
-    path("listauthor/", views.ListAuthorView.as_view(), name="listauthor"),
-    path("authorform/", views.author_fill, name="authorform"), 
-    path("authorvideo/", views.author_video.as_view(), name="authorvideo"), 
-    path("authorformset/", views.authorformset, name="authorformset"), 
+    path("currentday/", vroom_view.current_datetime, name="currentday"),
+    
+    path("daytemplate/", vroom_view.current_datetime_template, name="daytemplate"),
+    path("listcourse/", elearning_view.ListCourseView.as_view(), name="listcourse"),
+    path("register/", elearning_view.register_fill, name="registermember"),
+    path("manageauthor/", vroom_view.authorformset, name="authorformset"),
+    path("manageauthor_m/", vroom_view.authorformsetm, name="authorformsetm"),
+    
+    path("inquiry/", elearning_view.inquiry_fill, name="inquiry"),
+    path("authorform/", vroom_view.author_fill, name="addauthor"),
+    path("approveform/", elearning_view.approve_fill, name="approve"),
+    path("loginsys/", elearning_view.login_fill, name="login"),
+    path("logoutsys/", elearning_view.logout, name="logout"),
+    path("memberpage/", elearning_view.ListMyCourse, name="mycourse"),
+    path("inquiryform/", vroom_view.inquiry_fill, name="inquiryform"),
+    path("courseroom/<int:courseid>/<int:videoid>/", elearning_view.course_room, name="courseroom"),
+    path("payment/<int:courseid>/", elearning_view.payment_fill, name="payment"),
+    path("courseapprove/", elearning_view.courseapprove_fill, name="approvecourse"),
+    path("adminpage/", elearning_view.admin_manage, name="adminpage"),
+    path("coursedemoroom/<int:courseid>/<int:videoid>/", elearning_view.coursedemo_room, name="coursedemoroom"),
+    path("allcourse/",elearning_view.redirectView.as_view(), name="allcourse"),
+    path("createcourse/",elearning_view.create_course, name="createcourse"),
+    path("editcourse/<int:courseid>/",elearning_view.edit_course, name="editcourse"),
+    path("approvepublish/",elearning_view.approve_course, name="publishcourse"),
+    
+    path("createvideo/<int:courseid>/",elearning_view.create_video, name="createvideo"),
+    
+    path("getauthor/<int:authorid>/",vroom_view.author_detail, name="getauthor"),
+    
+    path("listcreatedcourse/",elearning_view.list_created_course, name="listcreatedcourse"),
+    path("editvideoclip/<int:courseid>/<int:videoid>/",elearning_view.edit_videoclip, name="editvideo"),
 
+    path("aboutus/",vroom_view.aboutus.as_view(),name="aboutus"),
+    path("courselist/",vroom_view.course_list.as_view(),name="course_list"),
+    path("author_video/",vroom_view.author_video.as_view(),name="author_video"),
+
+    path("delcourse/<int:courseid>/",elearning_view.del_course,name="deletecourse"),
+    path("listvideo/<int:courseid>/",elearning_view.list_created_video,name="listvideo"),
+    path("delvideo/<int:videoid>/",elearning_view.del_video,name="deletevideo"),
+    
+    path("api/onlinecourse/",elearning_view.ListOnlineCourseAPI.as_view(), name="querycourse"),
+    path("sql/",vroom_view.sql_example.as_view(), name="sql"),
+    path("<pk>/deletecourse/",elearning_view.course_delete_view.as_view(), name="del-course-view"),
+    path("memberlist/",vroom_view.member_list, name="listmember"),
+    path("canbeauthor/",vroom_view.member_author, name="canbeauthor"),
+    path("adminlist/",vroom_view.member_admin, name="listadmin"),
+    path("<pk>/listmemberm/", elearning_view.ShowMember.as_view(), name="membermix"),
+    path("listvideo/", vroom_view.ListVideoView.as_view(), name="listvideo"),
+    path("<slug:slug>", vroom_view.ListEachVideoView.as_view(), name="listvideoslug"),  # new
+    #path("api/video/", vroom_view.VideoAPI.as_view(),name="videoapi"),
+    path("stock/", include("vroom.urls")),
+    path("settings/", elearning_view.setting_info, name="setting"),
+    path("videoform/", vroom_view.video_fill, name="videoform"),
+    path("jsexample/", vroom_view.jsexample, name="jsexample"),
+    
+  #  path("getsheet/<str:stock>/<str:startdate>/<str:enddate>/<int:day>/<str:attinfo>/<str:graphtype>/<str:mlgraph>/", stock_view.sheet_stock.as_view(), name = "getsheet" ),
+    path("getsheet/", stock_view.sheet_stock.as_view(), name = "getsheet" ),
+    path("viewplot/<str:report_id>/", stock_view.show_plot, name = "showplot" ),
+    path("subscribe/", stock_view.Subscription, name = "subscribe" ),
+    path("tabview/<str:report_id>/<str:stock>/<str:startdate>/<str:enddate>/<int:day>/<int:fast>/<int:slow>/", stock_view.tab_view, name = "tabview" ),
+    path("validate/", elearning_view.mymodel_view, name="validate") , 
+    path("indicators/", stock_view.indicators, name="indicators"),  
+    path('proof/', elearning_view.proofview, name="proof view"),
+    path('interpret/<str:stock>/', stock_view.interpret_p1, name="interpretp1"),
+    path('interpretrsi/<str:stock>/', stock_view.interpret_p2, name="interpretrsi"),
+    path('workshop2_1/<str:stock>/<str:startdate>/<str:enddate>/', stock_view.workshop_2_1_view, name="interpretrsi"),
+    path('workshop2_2/<str:stock>/<str:startdate>/<str:enddate>/', stock_view.workshop_2_2_view, name="interpretrsi"),
+    path('workshop2_3/<str:stock>/<str:startdate>/<str:enddate>/', stock_view.workshop_2_3_view, name="interpretrsi"),
+    path('workshop2_4/<str:stock>/<str:startdate>/<str:enddate>/<int:pchg>/', stock_view.workshop_2_4_view, name="interpretrsi"), 
+    path('workshop2_5/<str:stock>/<str:startdate>/<str:enddate>/', stock_view.workshop_2_5_view, name="interpretrsi"), 
+    path('workshop2_6/<str:stock>/', stock_view.workshop_2_6_view, name="interpretrsi"), 
+    path('workshop2_7/<str:stock>/<str:startdate>/<str:enddate>/<int:lssma>/', stock_view.workshop_2_7_view, name="interpretrsi"), 
+    path('workshop2_8/<str:stock>/<str:startdate>/<str:enddate>/<str:period>/', stock_view.workshop_2_8_view, name="interpretrsi"), 
+    path('workshop2_9/<str:stock>/<str:startdate>/<str:enddate>/<str:timef>/', stock_view.workshop_2_9_view, name="interpretrsi"), 
+    path('workshop2_10/<str:stock>/<str:startdate>/<str:enddate>/', stock_view.workshop_2_10_view, name="interpretrsi"), 
+    path('next2u/', stock_view.sheet_next2u.as_view(), name="interpretrsi"), 
+    path('pdfview/', ebook_view.pdf_view_django, name="ebook"), 
+    path('profile/', elearning_view.myprofile, name="ebook"), 
+    path('subscription/', elearning_view.mysubscription, name="ebook"), 
+    path('ebook/', elearning_view.ebooklist, name="ebook"), 
+    path('carloan/', elearning_view.carloan, name="ebook"), 
+    path('login/', elearning_view.LoginAuthenAPI.as_view(), name="login"), 
+    path('api-token-auth/', views.obtain_auth_token),
+    path('login4/', elearning_view.Login4),
+    path('parcel/',elearning_view.parcel),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/protected/', elearning_view.ProtectedView.as_view(), name='protected_view'),
+    path('blog/', blog_view.PostList.as_view(), name='home'),
+    path('<slug:slug>/blog/', blog_view.PostDetail.as_view(), name='post_detail'),
+    path('summernote/', include('django_summernote.urls')),
 ]
+
+handler404 = vroom_view.Handler_404
+handler500 = vroom_view.Handler_500
+handler403 = vroom_view.Handler_403
+handler400 = vroom_view.Handler_400
+
+from django.conf import settings
+from django.conf.urls.static import static
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
